741c638bf37251c0e1cbc12597c5be4b66c8e266
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2023-04-07 20:47:08 +0300

    Add initial http fetch (#2)
    * Add 'http' package
    
    * Add network.client entitlement for macOS
    
    * Load & show response body
    
    * Add dependabot & gh pages workflow
    
    * Add missing indentation for macos debug profile
    
    * Fix baseHref in gh pages workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -32,12 +33,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var _response = '';
+  final String _fetchAddress = 'https://jsonplaceholder.typicode.com/albums/1';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> _fetchAlbum() async {
+    final response = await http.get(
+      Uri.parse(_fetchAddress),
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _response = response.body;
+      });
+    } else {
+      throw Exception('Failed to load album');
+    }
   }
 
   @override
@@ -50,20 +60,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text(_response),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _fetchAlbum,
+        tooltip: 'Fetch Album',
+        child: const Icon(Icons.refresh),
       ),
     );
   }
